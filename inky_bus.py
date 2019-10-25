649e0a3303d6d2c3f50766c01323455f1bd83db7
Ben Best <ben.best@olmgroup.com>
2019-10-26 00:48:27 +0100

    Feat: now sorted

--- inky_bus.py
+++ inky_bus.py
@@ -3,9 +3,10 @@ import requests
 
 resp = requests.get('https://api.tfl.gov.uk/StopPoint/490007732N/arrivals')
 if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for bus in resp.json():
+    raise ApiError('GET /arrivals/ {}'.format(resp.status_code))
+sortedArrival = resp.json()
+sortedArrival.sort(key=lambda k: k['timeToStation'], reverse=False)
+for bus in sortedArrival:
     
     minutes = bus['timeToStation']//60
     seconds = bus['timeToStation']%60
