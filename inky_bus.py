26fffa034b94fa052867904abbb97c67c71f0936
Ben Best <ben.best@olmgroup.com>
2019-10-26 00:09:37 +0100

    Chore; formatting

--- inky_bus.py
+++ inky_bus.py
@@ -1,13 +1,13 @@
 import requests
-from datetime import datetime
-from dateutil import parser
+
 
 resp = requests.get('https://api.tfl.gov.uk/StopPoint/490007732N/arrivals')
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 for bus in resp.json():
-    arrival = parser.parse(bus['expectedArrival'])
-    print('{} {}'.format(bus['lineName'], arrival.strftime("%H:%M:%S")))
-    #print('{} {}'.format(bus['lineName']))
-    #print('Bus - {}'.format(bus['lineNumber']))
+    
+    minutes = bus['timeToStation']//60
+    seconds = bus['timeToStation']%60
+    print('{} {:02d}mins {:02d}secs {}'.format(bus['lineName'], minutes, seconds, bus['destinationName']))
+   
