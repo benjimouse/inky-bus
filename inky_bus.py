40f4aa41945faa9abd12aec0a98674617ed4adba
Ben Best <ben.best@olmgroup.com>
2019-10-26 13:40:38 +0100

    Starting to add time.'

--- inky_bus.py
+++ inky_bus.py
@@ -1,14 +1,20 @@
 import requests
-
+from datetime import datetime, timezone
+from dateutil.parser import parser, isoparse
 
 resp = requests.get('https://api.tfl.gov.uk/StopPoint/490007732N/arrivals')
-if resp.status_code != 201:
+if resp.status_code != 200:
     raise ApiError('GET /arrivals/ {}'.format(resp.status_code))
 sortedArrival = resp.json()
 sortedArrival.sort(key=lambda k: k['timeToStation'], reverse=False)
+reRunTime = datetime.now(timezone.utc)
+print('{}'.format(reRunTime))
 for bus in sortedArrival:
-    
+   # if datetime.fromisoformat(bus['timeToLive']) < reRunTime
+    #    reRunTime = parser.parse(bus['timeToLive'])
+
     minutes = bus['timeToStation']//60
     seconds = bus['timeToStation']%60
     print('{} {:02d}mins {:02d}secs {}'.format(bus['lineName'], minutes, seconds, bus['destinationName']))
+    print('time to live {}'.format(isoparse(bus['timeToLive'])))
    
