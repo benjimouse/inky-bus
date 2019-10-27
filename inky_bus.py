d4a09bf26db34ed7ed8511df7abc73a9675b2576
Ben Best <ben.best@olmgroup.com>
2019-10-27 17:34:39 +0000

    Starting to look at times some more.

--- inky_bus.py
+++ inky_bus.py
@@ -2,19 +2,26 @@ import requests
 from datetime import datetime, timezone
 from dateutil.parser import parser, isoparse
 
-resp = requests.get('https://api.tfl.gov.uk/StopPoint/490007732N/arrivals')
-if resp.status_code != 200:
-    raise ApiError('GET /arrivals/ {}'.format(resp.status_code))
-sortedArrival = resp.json()
-sortedArrival.sort(key=lambda k: k['timeToStation'], reverse=False)
 reRunTime = datetime.now(timezone.utc)
-print('{}'.format(reRunTime))
-for bus in sortedArrival:
-   # if datetime.fromisoformat(bus['timeToLive']) < reRunTime
-    #    reRunTime = parser.parse(bus['timeToLive'])
 
-    minutes = bus['timeToStation']//60
-    seconds = bus['timeToStation']%60
-    print('{} {:02d}mins {:02d}secs {}'.format(bus['lineName'], minutes, seconds, bus['destinationName']))
-    print('time to live {}'.format(isoparse(bus['timeToLive'])))
-   
+def get_bus_time():
+    resp = requests.get('https://api.tfl.gov.uk/StopPoint/490007732N/arrivals')
+    if resp.status_code != 200:
+        raise ApiError('GET /arrivals/ {}'.format(resp.status_code))
+    sortedArrival = resp.json()
+    sortedArrival.sort(key=lambda k: k['timeToStation'], reverse=False)
+    
+    reRunTime = isoparse(sortedArrival[0]['timeToLive'])
+    print('{}'.format(reRunTime))
+    for bus in sortedArrival:
+        if isoparse(bus['timeToLive']) < reRunTime:
+            reRunTime = isoparse(bus['timeToLive'])
+            
+
+        minutes = bus['timeToStation']//60
+        seconds = bus['timeToStation']%60
+        print('{} {:02d}mins {:02d}secs {}'.format(bus['lineName'], minutes, seconds, bus['destinationName']))
+        print('time to live {}'.format(isoparse(bus['timeToLive'])))
+    
+
+get_bus_time()
