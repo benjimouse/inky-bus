2a6ec373c598d8a77392005c471afe4833c9fcbd
Ben Best <ben.best@olmgroup.com>
2019-10-27 18:18:05 +0000

    Ben

--- buses.py
+++ buses.py
@@ -4,14 +4,14 @@ from font_intuitive import Intuitive
 from inky import InkyPHAT
 
 inky_display = InkyPHAT("red")
-inky_display.set_border(inky_display.WHITE)
+inky_display.set_border(inky_display.RED)
 scale_size = 1
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
 
-message = "Hello, World!"
+message = "Hello, World!\nBen"
 w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
