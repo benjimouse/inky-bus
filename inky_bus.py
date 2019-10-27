2298f42755e62ec4a825f040442406947fcefe6e
Ben Best <ben.best@olmgroup.com>
2019-10-27 23:04:19 +0000

    Moving stuff around

--- inky_bus.py
+++ inky_bus.py
@@ -56,16 +56,17 @@ def displayOnInky(busTimes):
     img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
     draw = ImageDraw.Draw(img)
 
-    lastCheckFont = ImageFont.truetype(HankenGroteskBold, 10)
+    lastCheckSize = 10
+    lastCheckFont = ImageFont.truetype(HankenGroteskBold, lastCheckSize)
     lastCheckMessage = 'Last Check:{}\n'.format(_lastCheck.strftime('%H:%M:%S'))
-    messageFont = ImageFont.truetype(HankenGroteskBold, int(80 / (len(busTimes)+1)))
+    messageFont = ImageFont.truetype(HankenGroteskBold, int(80 / (len(busTimes))))
     message = formatMessage(busTimes)
     
     #Display at top left
     x = 0
     y = 0
     draw.text((x, y), lastCheckMessage, inky_display.RED, lastCheckFont)
-    draw.text((x, y+10), message, inky_display.BLACK, messageFont)
+    draw.text((x, y+lastCheckSize), message, inky_display.BLACK, messageFont)
     inky_display.set_image(img)
     inky_display.show()
 
